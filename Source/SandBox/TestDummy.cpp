4083aab4aada5578113df8148d5dfb4f4b00a5fb
Stewart Mckenzie <mckenzie.stewart.e@gmail.com>
2021-03-07 20:48:42 -0800

    Refactored my character classes added attack animations

--- Source/SandBox/TestDummy.cpp
+++ Source/SandBox/TestDummy.cpp
@@ -1,12 +1,42 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TestDummy.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+
+#include "Engine.h"
 
 // Sets default values
 ATestDummy::ATestDummy()
 {
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true;			 // Character moves in the direction of input...
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->JumpZVelocity = JumpForceZ;
+	GetCharacterMovement()->AirControl = 0.2f;
+
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character)
+	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	static ConstructorHelpers::FObjectFinder<UAnimMontage> MeleeSwordAttackMontageObject(TEXT("AnimMontage'/Game/Blueprints/TestDummy/Animations/MeleeSwordAttackMontage.MeleeSwordAttackMontage'"));
+
+	if (MeleeSwordAttackMontageObject.Succeeded())
+	{
+		MeleeSwordAttackMontage = MeleeSwordAttackMontageObject.Object;
+	}
 }
 
 // Called when the game starts or when spawned
@@ -33,16 +63,56 @@ void ATestDummy::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent
 	PlayerInputComponent->BindAxis(TEXT("LookUpRate"), this, &ATestDummy::LookUpRate);
 	PlayerInputComponent->BindAxis(TEXT("LookRightRate"), this, &ATestDummy::LookRightRate);
 	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &APawn::AddControllerYawInput);
+
+	//attack Functionality
+	PlayerInputComponent->BindAction(TEXT("Attack"), EInputEvent::IE_Pressed, this, &ATestDummy::AttackStart);
+	PlayerInputComponent->BindAction(TEXT("Attack"), EInputEvent::IE_Released, this, &ATestDummy::AttackEnd);
+}
+
+void ATestDummy::AttackStart()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Attack started"));
+	//Generate a random attack number between 1-4
+	int MontageSectionIndex = FMath::RandRange(1, 5);
+
+	//Create a string to reference the start an attack animation sections
+	FString MontageSection = "Start_" + FString::FromInt(MontageSectionIndex);
+	//Play that animations
+	PlayAnimMontage(MeleeSwordAttackMontage, 1.f, FName(*MontageSection));
+}
+
+void ATestDummy::AttackEnd()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Attack FInished"));
 }
 
 void ATestDummy::MoveForward(float AxisValue)
 {
-	AddMovementInput(GetActorForwardVector() * AxisValue);
+	if ((Controller != NULL) && (AxisValue != 0.0f))
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, AxisValue);
+	}
 }
 
 void ATestDummy::MoveRight(float AxisValue)
 {
-	AddMovementInput(GetActorRightVector() * AxisValue);
+	if ((Controller != NULL) && (AxisValue != 0.0f))
+	{
+		// find out which way is right
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get right vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// add movement in that direction
+		AddMovementInput(Direction, AxisValue);
+	}
 }
 
 void ATestDummy::LookUpRate(float AxisValue)
--- Source/SandBox/TestDummy.h
+++ Source/SandBox/TestDummy.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Character.h"
 #include "TestDummy.generated.h"
 
-UCLASS()
+UCLASS(config = Game)
 class SANDBOX_API ATestDummy : public ACharacter
 {
 	GENERATED_BODY()
@@ -15,22 +15,31 @@ public:
 	// Sets default values for this character's properties
 	ATestDummy();
 
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, Category = Animation, meta = (AllowPrivateAccess = "true"))
+	class UAnimMontage *MeleeSwordAttackMontage;
+
 protected:
 	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	virtual void
+	BeginPlay() override;
 
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
 
 private:
+	// player initiates attack
+	void AttackStart();
+	//initiated attack ends
+	void AttackEnd();
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
 	void LookUpRate(float AxisValue);
 	void LookRightRate(float AxisValue);
 	UPROPERTY(EditAnywhere)
 	float RotationRate = 10;
+	UPROPERTY(EditAnywhere)
+	float JumpForceZ = 600;
 };
